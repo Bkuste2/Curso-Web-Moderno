5176d48bf86b13088a8fbe5942c250fe32116e8a
Bkuste2 <igorribeiro632@gmail.com>
2022-06-09 00:36:09 -0300

    React

---
+++ react/exercicios/src/index.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import ReactDOM from 'react-dom'
+import Multiplos from "./componentes/Multiplos";
+import { BoaNoite, BoaTarde } from "./componentes/Multiplos";
+
+// Nomes de componentes precisam começar com letra maiúscula!!!!!!!!!!!
+
+// const elemento = <h1>React 2</h1>
+
+ReactDOM.render(
+    <Fragment>
+        <Multiplos.BoaTarde nome="Igor"/>
+        <BoaNoite nome="Igor"/>
+    </Fragment>
+,   document.getElementById('root'))
