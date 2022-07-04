425b9ac7c10941a22795437e2b1c3cce51f1efb1
Bkuste2 <igorribeiro632@gmail.com>
2022-07-04 00:42:32 -0300

    Understanding a little about modularization

--- next/fundamentos/src/pages/estiloso.jsx
+++ next/fundamentos/src/pages/estiloso.jsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
-import styles from '../styles/Estiloso.module.css'
+import styles, {roxo} from '../styles/Estiloso.module.css'
 
 const Estiloso = () => {
     return (
         <Layout titulo={"Exemplo de CSS MT LOUCO"}>
-            <div className={styles.roxo}>
+            <div className={roxo}>
                 <h1>Estilo usando CSS MODULE</h1>
             </div>
         </Layout>
